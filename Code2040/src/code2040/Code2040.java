dc58b0e3aabe998e0230fc931398b6b0af10b76e
Sam Gearou <samgearou@gmail.com>
2016-11-04 20:03:16 -0700

    Step 1

--- Code2040/src/code2040/Code2040.java
+++ Code2040/src/code2040/Code2040.java
@@ -1,8 +1,9 @@
 
 package code2040;
 
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import javax.net.ssl.HttpsURLConnection;
 import org.json.JSONObject;
 
 public class Code2040 {
@@ -13,8 +14,21 @@ public class Code2040 {
          json.put("token", "9be19107d85a1e9a2ab2912e6d0c2e95");
          json.put("github", "https://github.com/SamGearou/Code2040.git");
          URL url = new URL(urlString);
-         HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+         HttpURLConnection con = (HttpURLConnection) url.openConnection();
          con.setRequestMethod("POST");
          con.setRequestProperty("Content-Type", CONTENT);
+         // Send post request
+	 con.setDoOutput(true); //allows you to write data to the URL
+         DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+	 wr.writeBytes(json.toString());
+	 wr.flush();
+	 wr.close();
+         
+         int responseCode = con.getResponseCode();
+	 System.out.println("\nSending 'POST' request to URL : " + url);
+	 System.out.println("Post parameters : " + json.toString());
+	 System.out.println("Response Code : " + responseCode);
+         String response = con.getResponseMessage();
+         System.out.println(response);
     }
 }
